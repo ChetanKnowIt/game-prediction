cf8472b88ec49bfe349b6189f7b908ef67e2dc3c
ChetanKnowIt <chetanamrao174@gmail.com>
2023-04-16 12:26:28 +0530

    add label encode with correct methods

--- mymodelfile.py
+++ mymodelfile.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.pipeline import Pipeline
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 '''
 Modify following class to develop your model
@@ -17,24 +18,100 @@ class MyModel:
 		self._model = DummyRegressor(strategy = 'constant',
 									  constant = 10**6)
 
-	def fit(self, training_data):
+	def preprocess(self, training_data):
+		'''
+		i/p[0] ball by ball data
+		ID,innings,overs,ballnumber,batter,bowler,non-striker,extra_type,batsman_run,extras_run,total_run,non_boundary,isWicketDelivery,player_out,kind,fielders_involved,BattingTeam
+		i/p[1] result_df
+		ID,City,Date,Season,MatchNumber,Team1,Team2,Venue,TossWinner,TossDecision,SuperOver,WinningTeam,WonBy,Margin,method,Player_of_Match,Team1Players,Team2Players,Umpire1,Umpire2
+		op_df.columns ~ "venue", "innings", "batting_team", "bowling_team", "batsmen", "bowlers"
+		'''
+		#=========================== LOADING DATASET ======================================================
+		ball_df = training_data[0] #ball by ball df
+		result_df = training_data[1] # results df
+		#=========================== LOADING DATASET ======================================================
+		
 
-		# create training data
-		# create dummy data now
-		dummy_training_data = [["MA Chidambaram Stadium",1,"Mumbai Indians","Kolkata Knight Riders",
-								"Quinton de Kock, Rohit Sharma, Suryakumar Yadav",
-								"Harbhajan Singh, Varun Chakravarthy, Shakib Al Hasan, Pat Cummins"],
-								["MA Chidambaram Stadium",2,"Kolkata Knight Riders","Mumbai Indians",
-								"Harbhajan Singh, Varun Chakravarthy, Shakib Al Hasan, Pat Cummins",
-								"Quinton de Kock, Rohit Sharma, Suryakumar Yadav"]]
+		##=========================== UPDATING TEAM NAMES ===================================================
+		rename_dict = {'Deccan Chargers': 'Sunrisers Hyderabad' ,
+               'Gujarat Lions': 'Gujarat Titans',
+               'Kings XI Punjab': 'Punjab Kings', 'Delhi Daredevils': 'Delhi Capitals',
+               'Pune Warriors': 'Rising Pune Supergiant', 'Rising Pune Supergiants' : 'Rising Pune Supergiant'}
+		ball_df['BattingTeam'] = ball_df['BattingTeam'].map(rename_dict).fillna(ball_df['BattingTeam'])
+		# result_df = result_df.drop(['Date', 'Season',])
+		##=========================== UPDATING TEAM NAMES ===================================================
+
+
+		#=========================== BATSMAN DICTIONARY FOR LABEL ENCODING===========================
+		batsman_runs = ball_df.groupby(ball_df['batter']) \
+		.sum() \
+		.sort_values(by=['batsman_run'], ascending=True).reset_index()
+
+		#print(batsman_runs.info())
+		batsman_order = batsman_runs['batter']
+		batsman_runs_dict = dict(batsman_order)
+		batsman_runs_dict = {value: key for key, value in batsman_runs_dict.items()}
+		#print(batsman_runs_dict, len(batsman_runs_dict))
+		#=========================== BATSMAN DICTIONARY FOR LABEL ENCODING===========================
+		
+		#=========================== BOWLER DICTIONARY FOR LABEL ENCODING===========================
+		bowler_wickets = ball_df.groupby(ball_df['bowler']) \
+		.sum() \
+		.sort_values(by=['isWicketDelivery'], ascending=True).reset_index()
+
+		print(bowler_wickets.info())
+		bowler_order = bowler_wickets['bowler']
+		bowler_wickets_dict = dict(bowler_order)
+		bowler_wickets_dict = {value: key for key, value in bowler_wickets_dict.items()}
+		#=========================== BOWLER DICTIONARY FOR LABEL ENCODING===========================
+
+		#=========================== TEAM DICTIONARY FOR LABEL ENCODING===========================
+
+		
+
+		#=========================== TEAM DICTIONARY FOR LABEL ENCODING===========================
+
+
+		# ===========================ONLY 6 OVERS ARE REQUIRED===========================
+		ball_df = ball_df[(ball_df['overs'] < 6)]
+		# ===========================ONLY 6 OVERS ARE REQUIRED===========================
+
+		# ============================ LABEL ENCODING FOR BATSMAN/BOWLER/TEAM ================================
+		
+		ball_df['batter'] = ball_df['batter'].map(batsman_runs_dict).fillna(ball_df['batter'])
+		print(ball_df['batter'].value_counts())
+
+
+
+		# ============================ LABEL ENCODING FOR BATSMAN/BOWLER/TEAM ==========================================
+		
+		##=============CUSTOMIZING DATAFRAME FOR ONLY CURRENT MATCH TEAMS====================================
+		test_data = pd.read_csv('/var/test_file.csv', sep = ',')
+		Team1, Team2 = test_data['batting_team'][0], test_data['batting_team'][1]
+		ball_df = ball_df[(ball_df['BattingTeam']== str(Team1)) | (ball_df['BattingTeam'] == str(Team2))]
+		result_df = result_df[(result_df['Team1']== str(Team1)) | (result_df['Team1'] == str(Team2))]
+		##=============CUSTOMIZING DATAFRAME FOR ONLY CURRENT MATCH TEAMS====================================
+
+		#print(ball_df.info())
+
+
+
+		# result = pd.merge(ball_df, ball_df, on='ID', how='left')
+
+		pass
+
+	def fit(self, training_data):		
+		# preprocess
+		proc_training_data = self.preprocess(training_data)
+
 		dummy_training_labels = np.array([30,30]).reshape(-1,1)
 
-		training_data = pd.DataFrame(data=dummy_training_data,
+		trn_df = pd.DataFrame(data=proc_training_data,
 									 columns = ["venue", "innings", "batting_team", 
 									 			"bowling_team", "batsmen", "bowlers"])
 
 		# train the model
-		self._model.fit(dummy_training_data,dummy_training_labels)
+		self._model.fit(trn_df,dummy_training_labels)
 
 		return self
 		
